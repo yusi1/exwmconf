c5ac542de47e48cdc7ebdd674255dcaed5c245ef
Yusef Aslam <YUZi54780@outlook.com>
2022-08-02 13:30:02 +0100

    Update TempEL templates file

--- dot-emacs.d/yaslam-lisp/tempel-setup.el
+++ dot-emacs.d/yaslam-lisp/tempel-setup.el
@@ -13,16 +13,16 @@
 
 ;; Setup completion at point
 (defun tempel-setup-capf ()
-;; Add the Tempel Capf to `completion-at-point-functions'.
-;; `tempel-expand' only triggers on exact matches. Alternatively use
-;; `tempel-complete' if you want to see all matches, but then you
-;; should also configure `tempel-trigger-prefix', such that Tempel
-;; does not trigger too often when you don't expect it. NOTE: We add
-;; `tempel-expand' *before* the main programming mode Capf, such
-;; that it will be tried first.
-(setq-local completion-at-point-functions
-            (cons #'tempel-expand
-                  completion-at-point-functions)))
+  ;; Add the Tempel Capf to `completion-at-point-functions'.
+  ;; `tempel-expand' only triggers on exact matches. Alternatively use
+  ;; `tempel-complete' if you want to see all matches, but then you
+  ;; should also configure `tempel-trigger-prefix', such that Tempel
+  ;; does not trigger too often when you don't expect it. NOTE: We add
+  ;; `tempel-expand' *before* the main programming mode Capf, such
+  ;; that it will be tried first.
+  (setq-local completion-at-point-functions
+              (cons #'tempel-expand
+                    completion-at-point-functions)))
 
 (add-hook 'prog-mode-hook 'tempel-setup-capf)
 (add-hook 'text-mode-hook 'tempel-setup-capf)
