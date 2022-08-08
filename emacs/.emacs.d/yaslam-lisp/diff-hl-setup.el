98d469ebbf82db84adf5e69d284cd87733a1ce29
Yusef Aslam <YUZi54780@outlook.com>
2022-08-08 19:34:27 +0100

    Fix diff-hl in dired

--- emacs/.emacs.d/yaslam-lisp/diff-hl-setup.el
+++ emacs/.emacs.d/yaslam-lisp/diff-hl-setup.el
@@ -1,10 +1,11 @@
 (require 'diff-hl)
 
-(global-diff-hl-mode)
 (add-hook 'magit-pre-refresh-hook 'diff-hl-magit-pre-refresh)
 (add-hook 'magit-post-refresh-hook 'diff-hl-magit-post-refresh)
 
-(setq diff-hl-margin-mode t)
-(setq diff-hl-dired-mode t)
+;; (setq diff-hl-margin-mode t)
+(add-hook 'dired-mode-hook 'diff-hl-dired-mode)
+
+(global-diff-hl-mode)
 
 (provide 'diff-hl-setup)
