72e31cea68f48e8becf931c8be24553b7ab48d9e
Yusef Aslam <YUZi54780@outlook.com>
2022-08-08 22:43:55 +0100

    Theming tweaks

--- emacs/.emacs.d/yaslam-lisp/theming-setup.el
+++ emacs/.emacs.d/yaslam-lisp/theming-setup.el
@@ -1,3 +1,14 @@
+(require 'tooltip)
+(setq tooltip-delay 0.5)
+(setq tooltip-short-delay 0.5)
+(setq x-gtk-use-system-tooltips nil)
+(setq tooltip-frame-parameters
+      '((name . "tooltip")
+        (internal-border-width . 6)
+        (border-width . 0)
+        (no-special-glyphs . t)))
+(add-hook 'after-init-hook #'tooltip-mode)
+
 (require 'modus-themes)
 
 ;; Put theme customization here
