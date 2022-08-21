695835f45950ae603dac3724536cd7dd167bb6de
Yusef Aslam <YUZi54780@outlook.com>
2022-08-21 11:21:43 +0100

    notmuch-setup.el: Add a new function to notmuch-setup.el, and setup for a new package. init.el: New package

--- emacs/.emacs.d/yaslam-lisp/utility-packages/notmuch-setup.el
+++ emacs/.emacs.d/yaslam-lisp/utility-packages/notmuch-setup.el
@@ -1,4 +1,5 @@
 (require 'notmuch)
+(require 'notmuch-labeler)
 
 (defun notmuch-update-maildir ()
   "Call `notmuch new` to update maildir."
@@ -12,6 +13,19 @@
 		"new"))
     (display-buffer buffer)))
 
+(defun fetch-mail ()
+  "Call `getmail --rcfile personal -v` to fetch new emails."
+  (interactive)
+  (let ((name "fetch-mail")
+	(buffer "*fetch-mail-status*"))
+    (make-process
+     :name name
+     :buffer buffer
+     :command '("/usr/bin/getmail"
+		"--rcfile" "personal"
+		"-v"))
+    (display-buffer buffer)))
+
 (let ((map global-map))
   (define-key global-map (kbd "C-c e e") 'notmuch)
   (define-key global-map (kbd "C-c e u") 'notmuch-update-maildir))
@@ -23,5 +37,12 @@
 (setq message-kill-buffer-on-exit t)
 (setq notmuch-fcc-dirs "sent")
 (setq notmuch-show-logo nil)
+
+;; `notmuch-labeler' setup.
+(notmuch-labeler-rename "unread" "unread" ':foreground "aqua")
+(notmuch-labeler-rename "inbox" "inbox" ':foreground "green")
+(notmuch-labeler-rename "signed" "signed" ':foreground "pink")
+(notmuch-labeler-rename "urgent" "urgent" ':foreground "red")
+(notmuch-labeler-rename "read" "read" ':foreground "green")
 
 (provide 'notmuch-setup)
