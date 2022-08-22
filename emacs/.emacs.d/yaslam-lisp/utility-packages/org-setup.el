ff58ba0211ce283774ca31f6efd70bfce94fb8dd
Yusef Aslam <YUZi54780@outlook.com>
2022-08-22 11:06:36 +0100

    org-setup.el: Reorder configuration and add comments

--- emacs/.emacs.d/yaslam-lisp/utility-packages/org-setup.el
+++ emacs/.emacs.d/yaslam-lisp/utility-packages/org-setup.el
@@ -1,12 +1,15 @@
 (require 'org)
-(require 'org-superstar)
+(require 'org-modern)
+(require 'org-modern-indent)
 
 ;; Org mode hooks
 (add-hook 'org-mode-hook (lambda () (setq display-line-numbers-mode 'nil)))
 (add-hook 'org-mode-hook (lambda () (auto-fill-mode)))
-(add-hook 'org-mode-hook (lambda () (org-indent-mode t)))
-(add-hook 'org-mode-hook (lambda () (if (display-graphic-p)
-					(org-superstar-mode 1))))
+(add-hook 'org-mode-hook (lambda () (org-modern-indent-mode)))
+
+;; Enable `global-org-modern-mode' if we are on a graphical display.
+(if (display-graphic-p)
+    (global-org-modern-mode))
 
 ;; Global Org mode keybinds
 (let ((map global-map))
@@ -30,18 +33,79 @@
 
 ;; Configuration
 (visual-line-mode 1)
+
 ;; Improve org mode looks
-(setq org-startup-folded t)
-(setq org-startup-with-inline-images t)
-(setq org-image-actual-width '(600))
-(setq org-startup-indented t)
-(setq org-pretty-entities t)
-(setq org-hide-emphasis-markers nil)
-(setq org-hide-leading-stars nil)
-;; Don't align tags
-(setq org-auto-align-tags t)
-;; For navigation in the `org-goto' buffer
-(setq org-goto-auto-isearch nil)
+(setq
+ ;; Startup folded
+ org-startup-folded t
+ ;; Startup with inline images
+ org-startup-with-inline-images t
+ ;; Image width
+ org-image-actual-width '(600)
+ ;; Startup indented
+ org-startup-indented t
+ ;; Use UTF-8 characters for Org-mode
+ org-pretty-entities t
+ ;; Hide emphasis markers like =code= or ~code~ etc..
+ org-hide-emphasis-markers t
+ ;; Hide leading stars
+ org-hide-leading-stars t
+ ;; Use custom ellipsis for headings
+ org-ellipsis …
+ ;; Don't auto-align tags
+ org-auto-align-tags nil
+ ;; Don't auto-indent tags
+ org-tags-column 0
+ org-catch-invisible-edits 'show-and-error
+ ;; Use special ctrl-a/e keybinds that are content-aware.
+ setq org-special-ctrl-a/e t
+ ;; Respect content when inserting new headings.
+ org-insert-heading-respect-content t
+ ;; For navigation in the `org-goto' buffer
+ org-goto-auto-isearch nil)
+
+;; Enable `org-indent-mode'.
+(setq org-indent-mode t)
+
+;; `org-modern' configuration
+(setq
+ ;; Enable modern styling for code blocks and other blocks.
+ org-modern-block t
+ ;; Enable modern styling for tags.
+ org-modern-tag t
+ ;; Enable modern styling for todo labels.
+ org-modern-todo t
+ ;; Enable modern styling for tables.
+ org-modern-table t
+ ;; Enable modern styling for keywords.
+ org-modern-keyword t
+ ;; Enable modern styling for priority keywords.
+ org-modern-priority t
+ ;; Enable modern styling for timestamps.
+ org-modern-timestamp t
+ ;; Enable modern styling for statistics.
+ org-modern-statistics t
+ ;; Enable modern styling for horizontal rulers.
+ org-modern-horizontal-rule t)
+
+;; Use fixed pitch font for certain things
+(defun set-buffer-variable-pitch ()
+  "Set variable pitch, but set fixed-pitch for tables etc.."
+  (interactive)
+  (variable-pitch-mode t))
+
+(add-hook 'org-mode-hook 'set-buffer-variable-pitch)
+
+;; `org-agenda' styling
+(setq org-agenda-tags-column 0
+      org-agenda-block-separator ?─
+      org-agenda-time-grid
+      '((daily today require-timed)
+	(800 1000 1200 1400 1600 1800 2000)
+	" ┄┄┄┄┄ " "┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄")
+      org-agenda-current-time-string
+      "⭠ now ─────────────────────────────────────────────────")
+
 ;; Set org files directory
 (setq org-directory "~/Documents/Captures/GTD")
 ;; Set org agenda files location
@@ -49,13 +113,6 @@
       (org-dir-2 (eval `(concat ,org-directory "/Bills"))))
   (setq org-agenda-files `(,org-dir-1
 			   ,org-dir-2)))
-
-;; Disable leading stars
-(setq org-superstar-remove-leading-stars t)
-;; Org fold workaround for strange behaviour https://list.orgmode.org/87sfplyko3.fsf@localhost/
-;; (setq org-fold-core-style 'overlays)
-;; Indentation
-;; (setq org-indent-mode t)
 
 ;;; Setting up `org-crypt'
 (org-crypt-use-before-save-magic)
@@ -67,13 +124,6 @@
 
 (setq auto-save-default nil)
 
-;; Use fixed pitch font for certain things
-(defun set-buffer-variable-pitch ()
-  "Set variable pitch, but set fixed-pitch for tables etc.."
-  (interactive)
-  (variable-pitch-mode t))
-
-(add-hook 'org-mode-hook 'set-buffer-variable-pitch)
 
 ;; Configuring Org Babel
 (org-babel-do-load-languages 'org-babel-load-languages
@@ -87,18 +137,18 @@
 ;; https://orgmode.org/manual/Template-elements.html
 ;; http://howardism.org/Technical/Emacs/capturing-intro.html
 (setq org-capture-templates
-      '(("t" "Todo" entry (file+headline "~/Documents/GTD/Tasks/gtd.org" "Tasks")
+      '(("t" "Todo" entry (file+headline (eval (concat org-directory "/Tasks/gtd.org")) "Tasks")
          "* TODO %?\n  %i\n  %a")
 	("n" "Notes" plain (file "~/.notes")
 	 "\n* ENTER NOTE TITLE HERE%?\n")
 	("r" "Random Note" plain (file "~/.notes")
 	 "\n* Random Note\n%?"
 	 :empty-lines 0)
-	("p" "Project Idea" plain (file "~/Documents/GTD/Tasks/projects.org")
+	("p" "Project Idea" plain (file (eval (concat org-directory "/Tasks/projects.org")))
 	 "\n* TODO (ENTER PROJECT NAME HERE)%?\n")
-	("b" "Bills & Payments" plain (file "~/Documents/GTD/Bills/Bills.org")
+	("b" "Bills & Payments" plain (file (eval (concat org-directory"/Bills/Bills.org")))
 	 "\n* Bills & Payments\n** %?")
-	("j" "Journal" entry (file+headline "~/Documents/Journal/journal.org"
+	("j" "Journal" entry (file+headline "~/Documents/Captures/Journal/journal.org"
 					    "Personal Thoughts")
 	 "* %U :crypt:\n%?"
 	 :jump-to-captured t)
