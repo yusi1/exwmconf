b99ddce87f83996c0c60d5efb2b1e27e76add396
Yusef Aslam <YUZi54780@outlook.com>
2022-08-27 11:34:12 +0100

    early-init.el: misc tweaks

--- emacs/.emacs.d/early-init.el
+++ emacs/.emacs.d/early-init.el
@@ -5,6 +5,17 @@
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
+(tab-bar-mode -1)
+(tab-bar-history-mode -1)
 
-;; Load theme customizations
-;; (load-file (concat user-emacs-directory "yaslam-lisp/theming-setup.el"))
+;; Frame configuration
+;; Add frame borders and window dividers
+;; (modify-all-frames-parameters
+;;  '((right-divider-width . 0)
+;;    (internal-border-width . 0)))
+;; (dolist (face '(window-divider
+;;                 window-divider-first-pixel
+;;                 window-divider-last-pixel))
+;;   (face-spec-reset-face face)
+;;   (set-face-foreground face (face-attribute 'default :background)))
+;; (set-face-background 'fringe (face-attribute 'default :background))
