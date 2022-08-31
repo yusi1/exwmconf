066634538c228de2fc52079beca217d36b54bddd
Yusef Aslam <YUZi54780@outlook.com>
2022-08-31 21:09:04 +0100

    email-notifier.sh tweaks

--- etc-scripts/stuff/email-notifier.sh
+++ etc-scripts/stuff/email-notifier.sh
@@ -1,5 +1,5 @@
 msg=$(getmail --rcfile personal -v | grep "[1-9] messages*" | awk '{print "Email: " $1 " messages recieved"}')
 
 if [[ ! -z "${msg}" ]]; then
-    notify-send -t 10000 "${msg}"
+    /usr/bin/notify-send -t 10000 "${msg}"
 fi
