7bfb4e8eb5f677677432414f0b79812086bd496b
Yusef Aslam <YUZi54780@outlook.com>
2022-09-26 21:00:00 +0100

    emacs/.emacs.d/yaslam-lisp/essentials/theming-setup-ef.el: - Ef-themes customization

--- emacs/.emacs.d/yaslam-lisp/essentials/theming-setup-ef.el
+++ emacs/.emacs.d/yaslam-lisp/essentials/theming-setup-ef.el
@@ -16,15 +16,26 @@
 
 ;; Heading customization
 (setq ef-themes-headings
-      (quote ((1 . (light variable-pitch 1.4))
+      (quote ((1 . (regular 1.4))
 	      (2 . (monochrome 1.2))
 	      (t . (semibold)))))
 
+;; Use mixed-pitch fonts for UI
+(setq ef-themes-mixed-fonts t)
+;; (setq ef-themes-mixed-fonts nil)
+
 ;; Which themes to toggle
-(setq ef-themes-toggle '(ef-deuteranopia-dark ef-deuteranopia-light))
+;; (setq ef-themes-to-toggle '(ef-deuteranopia-dark ef-deuteranopia-light))
+(setq ef-themes-to-toggle '(ef-duo-dark ef-duo-light))
+;; (setq ef-themes-to-toggle '(ef-day ef-dark))
+
+(gkey "<f12>" 'ef-themes-toggle)
 
 ;; Load the theme of choice:
-(load-theme 'ef-deuteranopia-dark :no-confirm)
+;; (load-theme 'ef-deuteranopia-dark :no-confirm)
+;; (load-theme 'ef-day :no-confirm)
+;; (load-theme 'ef-dark :no-confirm)
+(load-theme 'ef-duo-dark)
 
 ;; The themes we provide:
 ;;
