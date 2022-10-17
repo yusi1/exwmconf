7f09ce35a7762adc8cb57de26be096bdd75efbad
Yusef Aslam <YUZi54780@outlook.com>
2022-10-17 21:32:24 +0100

    * .gitignore: - Don't track gkrellm2 data directory under git. * theming-setup-doom.el: - Comment out theme.

--- emacs/.emacs.d/yaslam-lisp/essentials/theming-setup-doom.el
+++ emacs/.emacs.d/yaslam-lisp/essentials/theming-setup-doom.el
@@ -4,5 +4,6 @@
 ;; (load-theme 'doom-zenburn t)
 ;; (load-theme 'doom-solarized-dark-high-contrast t)
 ;; (load-theme 'doom-solarized-light t)
+;; (load-theme 'doom-palenight t)
 
 (provide 'theming-setup-doom)
