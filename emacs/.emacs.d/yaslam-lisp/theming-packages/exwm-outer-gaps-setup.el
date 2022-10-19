724c7591b88ccfecea79e59377a0a94961d1de8d
Yusef Aslam <YUZi54780@outlook.com>
2022-10-19 20:08:57 +0100

    * exwm-outer-gaps.el: Add `with-eval-after-load' form.

--- emacs/.emacs.d/yaslam-lisp/theming-packages/exwm-outer-gaps-setup.el
+++ emacs/.emacs.d/yaslam-lisp/theming-packages/exwm-outer-gaps-setup.el
@@ -1,12 +1,13 @@
 (require 'exwm)
-
+(require 'exwm-outer-gaps)
 (add-hook 'exwm-init-hook (lambda ()
 			    (progn
 			      (require 'exwm-outer-gaps)
 			      (exwm-outer-gaps-mode 1))))
 
-(unless (get 'exwm-input-global-key 'saved-value)
-  (add-to-list 'exwm-input-global-keys
-	       `([?\s-G] . exwm-outer-gaps-mode)))
+(with-eval-after-load 'exwm
+  (unless (get 'exwm-input-global-keys 'saved-value)
+    (add-to-list 'exwm-input-global-keys
+		 '([?\s-G] . exwm-outer-gaps-mode))))
 
 (provide 'exwm-outer-gaps-setup)
