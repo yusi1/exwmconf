7b6e98c79731e049da7f2d672fe3f23e46f0fa7f
Yusef Aslam <YUZi54780@outlook.com>
2022-10-22 19:14:19 +0100

    * ysz-theme.el: Add cursory package and configuration.

--- emacs-usepackage/.emacs.d/site-lisp/ysz-theme.el
+++ emacs-usepackage/.emacs.d/site-lisp/ysz-theme.el
@@ -1,5 +1,38 @@
 ;;; Theming configurations --- ysz-theme.el
 
+(use-package cursory
+  :straight t
+  :bind (("C-c C-p" . cursory-set-preset))
+  :config
+  (setq cursory-presets
+	'((bar
+           :cursor-type (bar . 2)
+           :cursor-in-non-selected-windows hollow
+           :blink-cursor-blinks 10
+           :blink-cursor-interval 0.5
+           :blink-cursor-delay 0.2)
+	  (slow-bar
+	   :cursor-type (bar . 2)
+	   :cursor-in-non-selected-windows hollow
+	   :blink-cursor-blinks 3
+	   :blink-cursor-interval 0.8
+	   :blink-cursor-delay 0.5)
+          (box
+           :cursor-type box
+           :cursor-in-non-selected-windows hollow
+           :blink-cursor-blinks 10
+           :blink-cursor-interval 0.5
+           :blink-cursor-delay 0.2)
+          (underscore
+           :cursor-type (hbar . 3)
+           :cursor-in-non-selected-windows hollow
+           :blink-cursor-blinks 50
+           :blink-cursor-interval 0.2
+           :blink-cursor-delay 0.2)))
+  (setq cursory-latest-state-file (locate-user-emacs-file "cursory-latest-state"))
+  (cursory-set-preset (or (cursory-restore-latest-preset) 'box))
+  (add-hook 'kill-emacs-hook #'cursory-store-latest-preset))
+
 (use-package modus-themes
   :straight t
   :demand t
