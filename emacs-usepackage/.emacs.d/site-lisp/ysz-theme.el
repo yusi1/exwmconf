446fdc3910311afc9c62b17c532bf24e795c4c4d
Yusef Aslam <YUZi54780@outlook.com>
2022-10-22 04:48:29 +0100

    * site-lisp/ysz-theme.el: Add modus-themes-headings config.

--- emacs-usepackage/.emacs.d/site-lisp/ysz-theme.el
+++ emacs-usepackage/.emacs.d/site-lisp/ysz-theme.el
@@ -6,6 +6,9 @@
   :bind (("<f12>" . modus-themes-toggle))
   :config
   (setq modus-themes-mode-line '(3d accented))
+  (setq modus-themes-headings '((1 . (light variable-pitch 1.5))
+				(2 . (monochrome 1.05))
+				(t . (semibold))))
   (modus-themes-load-vivendi))
 
 (provide 'ysz-theme)
