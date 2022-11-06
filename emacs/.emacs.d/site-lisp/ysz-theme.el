7f82687f24540f658614d4538be1b9420c49cb82
Yusef Aslam <YUZi54780@outlook.com>
2022-11-06 12:29:14 +0000

    emacs (ysz-theme.el): - Add modus-themes, green-is-the-new-black-theme, doom-themes, doom-modeline and fontaine. - Change location of modus-themes config. - Add diredfl.

--- emacs/.emacs.d/site-lisp/ysz-theme.el
+++ emacs/.emacs.d/site-lisp/ysz-theme.el
@@ -1,5 +1,118 @@
 ;;; Theming configurations --- ysz-theme.el
 
+(use-package modus-themes
+  :straight t
+  :demand t
+  :bind (("<f12>" . modus-themes-toggle))
+  :config
+  (setq
+   ;; modus-themes-mode-line '(3d accented)
+   modus-themes-mode-line '(3d)
+
+   modus-themes-headings '((1 . (light variable-pitch 1.5))
+			   (2 . (monochrome 1.05))
+			   (t . (semibold)))
+   modus-themes-org-blocks 'gray-background
+   modus-themes-mixed-fonts t)
+  ;;;;;;;;;;;;;;
+  ;; (if (not (null (getenv "XDG_CURRENT_DESKTOP")))
+  ;;     (if (string-match-p "dark"
+  ;; 			  (shell-command-to-string "gsettings get org.gnome.desktop.interface color-scheme"))
+  ;; 	  (modus-themes-load-vivendi)
+  ;; 	(modus-themes-load-operandi))
+  ;;   (modus-themes-load-vivendi))
+  (modus-themes-load-vivendi))
+
+;; (use-package green-is-the-new-black-theme
+;;   :straight '(green-is-the-new-black-emacs
+;; 	      :type git :host github
+;; 	      :repo "fredcamps/green-is-the-new-black-emacs")
+;;   :config
+;;   (load-theme 'green-is-the-new-black t))
+
+;; (use-package doom-themes
+;;   :straight t
+;;   :config
+;;   ;; Global settings (defaults)
+;;   (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+;;         doom-themes-enable-italic t) ; if nil, italics is universally disabled
+;;   (load-theme 'doom-one t)
+
+;;   ;; Enable flashing mode-line on errors
+;;   ;; (doom-themes-visual-bell-config)
+;;   ;; ;; Enable custom neotree theme (all-the-icons must be installed!)
+;;   ;; (doom-themes-neotree-config)
+;;   ;; ;; or for treemacs users
+;;   ;; (setq doom-themes-treemacs-theme "doom-atom") ; use "doom-colors" for less minimal icon theme
+;;   ;; (doom-themes-treemacs-config)
+;;   ;; Corrects (and improves) org-mode's native fontification.
+;;   (doom-themes-org-config))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(use-package doom-modeline
+  :straight t
+  :config
+  (setq doom-modeline-icon nil)
+  (doom-modeline-mode 1))
+
+(use-package fontaine
+  :straight t
+  :demand t
+  :bind (("C-c f" . fontaine-set-preset)
+	 ("C-c F" . fontaine-set-face-font)
+	 ("C-c r" . (lambda () (interactive)
+		      (fontaine-set-preset
+		       (fontaine-restore-latest-preset)))))
+  :config
+  ;; whether fontaine is enabled
+  (defvar fontaine-enabled nil "Whether fontaine is enabled or not.")
+  (setq fontaine-enabled nil)
+  
+  (setq fontaine-latest-state-file
+	(locate-user-emacs-file "fontaine-latest-state.eld"))
+  (setq fontaine-presets
+	'((iosevka-comfy-regular
+	   :default-family "Iosevka Comfy Fixed"
+	   :default-height 160
+	   :variable-pitch-weight bold
+	   :bold-family nil
+	   :bold-weight bold
+	   :line-spacing 1)
+	  (iosevka-comfy-small
+	   :default-family "Iosevka Comfy Fixed"
+	   :default-height 140
+	   :variable-pitch-weight bold
+	   :bold-family nil
+	   :bold-weight bold
+	   :line-spacing 1)
+	  (terminus
+	   :default-family "Terminus"
+	   :default-height 160
+	   :default-weight bold)
+	  (monoid-regular
+	   :default-family "Monoid"
+	   :default-height 160
+	   :default-weight regular)
+	  (monoid-medium
+	   :default-family "Monoid"
+	   :default-height 140
+	   :default-weight regular)
+	  (monoid-small
+	   :default-family "Monoid"
+	   :default-height 120
+	   :default-weight regular)
+	  (t
+	   :default-family "FiraCode"
+	   :default-weight normal
+	   :default-height 140
+	   ;; :variable-pitch-family "Iosevka Comfy Motion Duo"
+	   :variable-pitch-family "Iosevka Aile"
+	   :variable-pitch-weight nil
+	   :variable-pitch-height 1.0)))
+  (if fontaine-enabled
+      (if (display-graphic-p)
+	  (fontaine-set-preset (or (fontaine-restore-latest-preset) 'regular))))
+  (add-hook 'kill-emacs-hook #'fontaine-store-latest-preset))
+
 (use-package cursory
   :straight t
   :bind (("C-c C-p" . cursory-set-preset))
@@ -33,18 +146,10 @@
   (cursory-set-preset (or (cursory-restore-latest-preset) 'box))
   (add-hook 'kill-emacs-hook #'cursory-store-latest-preset))
 
-(use-package modus-themes
+(use-package diredfl
   :straight t
-  :demand t
-  :bind (("<f12>" . modus-themes-toggle))
   :config
-  (setq modus-themes-mode-line '(3d accented)
-	modus-themes-headings '((1 . (light variable-pitch 1.5))
-				(2 . (monochrome 1.05))
-				(t . (semibold)))
-	modus-themes-org-blocks 'gray-background)
-  ;;;;;;;;;;;;;;
-  (modus-themes-load-vivendi))
+  (diredfl-global-mode 1))
 
 (provide 'ysz-theme)
 ;;; ysz-theme.el ends here
