e68373e3c8c62a0de918a121d9bac93858e97797
Yusef Aslam <YUZi54780@outlook.com>
2022-11-06 12:41:06 +0000

    stumpwm (init.lisp): Change font weight.

--- stumpwm.d/.stumpwm.d/init.lisp
+++ stumpwm.d/.stumpwm.d/init.lisp
@@ -209,7 +209,7 @@
 (define-key *root-map* (kbd "M-i") "imdb")
 
 ;; Message window font
-(set-font "-xos4-terminus-medium-r-normal--14-140-72-72-c-80-iso8859-15")
+(set-font "-xos4-terminus-bold-r-normal--14-140-72-72-c-80-iso8859-15")
 
 ;;; Define window placement policy...
 
