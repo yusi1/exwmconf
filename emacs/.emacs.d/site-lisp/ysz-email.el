815c6f3864334ef41aa015cc621d95a367aed16c
Yusef Aslam <YUZi54780@outlook.com>
2022-11-06 12:27:02 +0000

    emacs (ysz-email.el): - Migrate to general for keybinds. - Add useful notmuch functions from previous emacs config. - Correct typo in `user-mail-address'.

--- emacs/.emacs.d/site-lisp/ysz-email.el
+++ emacs/.emacs.d/site-lisp/ysz-email.el
@@ -1,12 +1,56 @@
 ;;; Email configurations (notmuch et al.) --- ysz-email.el
 
+(use-package general
+  :straight t)
+
 (use-package notmuch
   :straight t
   :config
-  (keymap-set global-map "C-c e" 'notmuch)
+  (defun notmuch-update-maildir ()
+    "Call `notmuch new` to update maildir."
+    (interactive)
+    (let ((name "maildir-update")
+	  (buffer "*maildir-update*"))
+      (make-process
+       :name name
+       :buffer buffer
+       :command '("notmuch"
+		  "new"))
+      ))
+  (defun display-maildir-update-buffer ()
+    "Display the maildir update buffer."
+    (interactive)
+    (when (notmuch-update-maildir)
+      (let ((buffer "*maildir-update*"))
+	(if (get-buffer buffer)
+            (display-buffer buffer `((display-buffer-in-side-window)
+				     (side . bottom)
+				     (window-height . 10)))))))
+  (defun fetch-mail ()
+    "Call `getmail --rcfile personal -v` to fetch new emails."
+    (interactive)
+    (let ((name "fetch-mail")
+	  (buffer "*fetch-mail-status*"))
+      (make-process
+       :name name
+       :buffer buffer
+       :command '("/usr/bin/getmail"
+		  "--rcfile" "personal"
+		  "-v"))
+      (display-buffer buffer)))
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  (general-def notmuch-hello-mode-map
+    "C-c C-u" 'display-maildir-update-buffer)
+  (general-def global-map
+    :prefix "C-c e"
+    "e" 'notmuch
+    "u" 'notmuch-update-maildir)
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  (add-hook 'notmuch-hello-refresh-hook 'notmuch-update-maildir)
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
   (setq mail-host-address "YUZi54780@outlook.com")
   (setq user-full-name "Yusef Aslam")
-  (setq user-mail-adress "YUZi54780@outlook.com")
+  (setq user-mail-address "YUZi54780@outlook.com")
   (setq mail-user-agent 'message-user-agent)
   (setq message-kill-buffer-on-exit t)
   (setq notmuch-fcc-dirs "sent")
