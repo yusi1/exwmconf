436249814b7fcd1a66199d7c0cd7365fecd1393e
Yusef Aslam <YUZi54780@outlook.com>
2022-11-12 18:28:33 +0000

    emacs (ysz-media.el): - Comment out `emms' use-package declaration. - Add `simple-mpc' use-package declaration.

--- emacs/.emacs.d/site-lisp/ysz-media.el
+++ emacs/.emacs.d/site-lisp/ysz-media.el
@@ -1,23 +1,23 @@
 ;;; Multimedia configurations for Emacs (EMMS et. al) --- ysz-media.el
 
-(use-package emms
-  :straight t
-  :config
-  (require 'emms-setup)
-  (emms-all)
-  (emms-default-players)
+;; (use-package emms
+;;   :straight t
+;;   :config
+;;   (require 'emms-setup)
+;;   (emms-all)
+;;   (emms-default-players)
+;;   (require 'emms-player-mpd)
+;;   (setq emms-mpd-server-name "localhost")
+;;   (setq emms-mpd-server-port "6600")
+;;   (add-to-list 'emms-player-list 'emms-player-mpd)
+;;   (emms-cache-set-from-mpd-all)
+;;   (emms-player-mpd-connect)
+;;   (require 'emms-volume)
+;;   (setq emms-volume-change-function 'emms-volume-mpd-change)
+;;   (setq emms-source-file-default-directory "~/Music/"))
 
-  (require 'emms-player-mpd)
-  (setq emms-mpd-server-name "localhost")
-  (setq emms-mpd-server-port "6600")
-  (add-to-list 'emms-player-list 'emms-player-mpd)
-  (emms-cache-set-from-mpd-all)
-  (emms-player-mpd-connect)
-
-  (require 'emms-volume)
-  (setq emms-volume-change-function 'emms-volume-mpd-change)
-
-  (setq emms-source-file-default-directory "~/Music/"))
+(use-package simple-mpc
+  :straight t)
 
 (provide 'ysz-media)
 ;; ysz-media.el ends here
