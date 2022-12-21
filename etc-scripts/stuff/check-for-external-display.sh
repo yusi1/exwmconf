2ab6ca68f27e24ffcf93032fc938cd99b259d165
Yusef Aslam <YUZi54780@outlook.com>
2022-12-21 17:25:52 +0000

    etc-scripts (check-for-external-display.sh): Change refresh rate, remove whitespace.

--- etc-scripts/stuff/check-for-external-display.sh
+++ etc-scripts/stuff/check-for-external-display.sh
@@ -5,7 +5,7 @@ ext_display="HDMI1"
 check=$(xrandr -q | grep "$ext_display connected")
 
 if [[ ${check} ]]; then
-    xrandr --output $ext_display --primary --mode 1920x1080 --rate 120
+    xrandr --output $ext_display --primary --mode 1920x1080 --rate 60 
     xrandr --output $int_display --off
 else
     xrandr --output $int_display --primary --mode 1366x768 --rate 60
