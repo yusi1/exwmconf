df5a18a6a17658003bb208fac024d98e4fb09c84
Yusef Aslam <YUZi54780@outlook.com>
2023-02-09 20:42:15 +0000

    doom-emacs-v2 (init.el): Add +contacts to org package, remove org-msg support from mu4e package.

--- doom-emacs-v2/.doom.d/init.el
+++ doom-emacs-v2/.doom.d/init.el
@@ -153,7 +153,7 @@
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       org               ; organize your plain life in plain text
+       (org +contacts)     ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
@@ -177,7 +177,8 @@
        ;;zig               ; C, but simpler
 
        :email
-       (mu4e +org +gmail)
+       ;; (mu4e +org +gmail)
+       (mu4e +gmail)
        ;;notmuch
        ;;(wanderlust +gmail)
 
