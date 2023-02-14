3ad6f1994cea5736a3ea243db3191b53e976f131
Yusef Aslam <YUZi54780@outlook.com>
2023-02-14 16:41:40 +0000

    doom-emacs-v2 (.doom.d/custom.el): Changes.

--- doom-emacs-v2/.doom.d/custom.el
+++ doom-emacs-v2/.doom.d/custom.el
@@ -3,6 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-safe-remote-resources
+   '("\\`https://fniessen\\.github\\.io/org-html-themes/org/readtheorg\\.setup\\'"))
  '(smtpmail-smtp-server "smtp.gmail.com")
  '(smtpmail-smtp-service 25))
 (custom-set-faces
