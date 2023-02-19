ac5023b30f52ca574233fc34e27c9fb36035bd8f
Yusef Aslam <YUZi54780@outlook.com>
2023-02-19 18:32:25 +0000

    config.el: uncomment 'vertico-multiform-commands'

--- doom-emacs-v2/.doom.d/config.el
+++ doom-emacs-v2/.doom.d/config.el
@@ -497,8 +497,8 @@ QUERY is the query to search for in the logs."
     "C-c M-F" 'vertico-multiform-flat
     "C-c M-R" 'vertico-multiform-reverse
     "C-c M-U" 'vertico-multiform-unobtrusive)
-  ;; (setq vertico-multiform-commands
-  ;;       '((consult-imenu buffer)
+  (setq vertico-multiform-commands
+        '((consult-imenu buffer)))
   ;;         (execute-extended-command flat)))
   (vertico-multiform-mode t))
 
