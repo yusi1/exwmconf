d153568e7704064619f671aeeda572b9e104b3b1
Yusef Aslam <YUZi54780@outlook.com>
2023-05-30 14:13:14 +0100

    xmonad.hs: add saveWindow and raiseSaved

--- xmonad-minimal/.config/xmonad/xmonad.hs
+++ xmonad-minimal/.config/xmonad/xmonad.hs
@@ -8,8 +8,11 @@ import XMonad.Hooks.EwmhDesktops
 import XMonad.Hooks.StatusBar
 import XMonad.Hooks.StatusBar.PP
 import XMonad.Util.EZConfig
+import qualified XMonad.Util.ExtensibleState as XS
 import XMonad.Layout.MultiToggle
 import XMonad.Layout.MultiToggle.Instances
+import qualified Data.Set as S
+import Data.Foldable (for_)
 import Data.Functor
 
 mySB = statusBarProp "xmobar" (pure xmobarPP)
@@ -21,6 +24,7 @@ main = xmonad . docks . ewmhFullscreen . ewmh $ withEasySB mySB defToggleStrutsK
           , borderWidth = 2
           , manageHook = myManageHook
           , layoutHook = myLayoutHook
+          , logHook = raiseSaved
           }
           `additionalKeysP`
           [ ("M-p", spawn "dmenu_run -fn 'DejaVu Sans Mono:pixelsize=18' -nf 'gray' -nb 'black' -sb 'red' -sf 'white'")
@@ -40,6 +44,26 @@ myLayoutHook = mkToggle (FULL ?? EOT) $ avoidStruts (tiled ||| Mirror tiled ||| 
      -- Percent of screen to increment by when resizing panes
      delta   = 3/100
 
+-- provided by geekosaur on IRC
+data SavedWindow = SavedWindow (S.Set Window) deriving (Read, Show)
+
+-- provided by geekosaur on IRC
+instance ExtensionClass SavedWindow where
+  initialValue = SavedWindow S.empty
+  extensionType = PersistentExtension
+
+-- use in ManageHook: className =? "whatever" --> saveWindow
+-- provided by geekosaur on IRC
+saveWindow :: ManageHook
+saveWindow = ask >>= \w -> liftX (XS.modify (\(SavedWindow s) -> SavedWindow (S.insert w s))) >> doIgnore
+
+-- use in logHook: no parameters
+-- provided by geekosaur on IRC
+raiseSaved :: X ()
+raiseSaved = do
+  SavedWindow s <- XS.get
+  withDisplay $ \d -> for_ s $ io . raiseWindow d
+
 -- this lets you have a floating window that is fully ignored
 -- but also lets you control the placement of the ignored floating window
 -- thanks to [Leary] on #xmonad for this
@@ -50,7 +74,5 @@ mapAt r = ask >>= \w -> liftX (tileWindow w r $> mempty)
 
 myManageHook :: ManageHook
 myManageHook = composeAll $
-               [ className =? "XClock" --> mapAt (Rectangle 1760 30 150 150) <> doIgnore
-                                                                             <> doRaise
-               , className =? "XEyes" --> mapAt (Rectangle 1860 1020 60 60) <> doIgnore
-                                                                            <> doRaise ] 
+               [ className =? "XClock" --> mapAt (Rectangle 1760 30 150 150) <> saveWindow
+               , className =? "XEyes" --> mapAt (Rectangle 1860 1020 60 60) <> saveWindow ]
