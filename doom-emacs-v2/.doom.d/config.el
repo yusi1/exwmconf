26e03dd0922d1107364a674fc2cacafab6d908a8
Yusef Aslam <YUZi54780@outlook.com>
2023-05-30 14:38:13 +0100

    config.el: change 'doom-font' variable

--- doom-emacs-v2/.doom.d/config.el
+++ doom-emacs-v2/.doom.d/config.el
@@ -24,7 +24,7 @@
 ;; See 'C-h v doom-font' for documentation and more examples of what they
 ;; accept. For example:
 ;;
-(setq doom-font "DejaVu Sans Mono-16")
+(setq doom-font "PragmataPro for Powerline-18")
 ;;
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
