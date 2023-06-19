648901172f363386500a92681aae4359fa359954
Yusef Aslam <YUZi54780@outlook.com>
2023-06-19 20:50:22 +0100

    Squashed 'dotfile-init-scripts/' changes from 8d62113..746cb3e
    746cb3e Add unlink-dotfiles.sh
    
    git-subtree-dir: dotfile-init-scripts
    git-subtree-split: 746cb3eed0dc6f4a9ad9d3068b2f02020a3831f6

---
+++ unlink-dotfiles.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+DOTFILES="$HOME/.dotfiles"
+
+cd $DOTFILES
+echo -e "\n[Linked files]"
+find $HOME -type l | xargs readlink -- | grep -i dotfiles | sed 's/\.\.\///1' | sed 's/.dotfiles\///g' | sort
+
+echo -e "\n[Select packages to UN-install]"
+
+PACKAGES_LIST=()
+
+select SELECTION in $(find $HOME -type l | xargs readlink -- | grep -i dotfiles | sed 's/\.\.\///1' | sed 's/.dotfiles\///g' | sed 's/\/.*//g' | sort -u | sort && echo -e "[EXIT]")
+do
+	[[ $SELECTION == "[EXIT]" ]] && break
+	PACKAGES_LIST+=($SELECTION)
+	echo "Selected: ${PACKAGES_LIST[@]}"
+done
+
+for PACKAGE in ${PACKAGES_LIST[@]}
+do
+	printf "\nUnlinking %s from $HOME\n" "$PACKAGE"
+	stow -D "$PACKAGE" -t "$HOME" -v
+done
