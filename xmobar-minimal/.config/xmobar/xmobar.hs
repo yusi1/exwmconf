57461323b905859f98ea32c5e4031c683ea93eff
Yusef Aslam <YUZi54780@outlook.com>
2023-06-26 14:26:02 +0100

    xmobar.hs: add XMONAD_TRAYPAD

--- xmobar-minimal/.config/xmobar/xmobar.hs
+++ xmobar-minimal/.config/xmobar/xmobar.hs
@@ -9,12 +9,13 @@ config =
       alpha = 200,
       commands =
         [ Run XMonadLog,
+          Run $ XPropertyLog "_XMONAD_TRAYPAD",
           Run $ Memory ["t", "Mem: <usedratio>%"] 10,
           Run $ Kbd [],
           Run $ Date "\xf00f0 %a %_d %b %Y <fc=#ee9a00>%H:%M:%S</fc>" "date" 10,
           Run Locks
         ],
-      template = "%XMonadLog% }{ [%locks%] %kbd% | %date% | %memory%",
+      template = "%XMonadLog% }{ [%locks%] %kbd% | %date% | %memory% %_XMONAD_TRAYPAD%",
       alignSep = "}{"
     }
 
