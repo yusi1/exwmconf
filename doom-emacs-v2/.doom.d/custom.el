5de2167044e37ab4447f00ee3b3d27632b7888d2
Yusef Aslam <YUZi54780@outlook.com>
2023-06-27 11:46:04 +0100

    custom.el: Custom changes

--- doom-emacs-v2/.doom.d/custom.el
+++ doom-emacs-v2/.doom.d/custom.el
@@ -4,9 +4,11 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("f64189544da6f16bab285747d04a92bd57c7e7813d8c24c30f382f087d460a33" "aec7b55f2a13307a55517fdf08438863d694550565dee23181d2ebd973ebd6b8" "e5a748cbefd483b74b183d7da4fca6228207a6bf9be9792dc85403a186724e1f" "9225db6c746ab38bdfe14af45aaf0549dc9d6bca73ffe81fd9e955f843d2e095" "68b35e92f9daa37685218bd11aa5307140a0ec4c8fd17142a83457619e7b1240" "bcfeecf5f2ee0bbc64450f7c5155145d8d2c590b1310a898c505f48b4b5f4c75" "02f57ef0a20b7f61adce51445b68b2a7e832648ce2e7efb19d217b6454c1b644" "467dc6fdebcf92f4d3e2a2016145ba15841987c71fbe675dcfe34ac47ffb9195" "51c71bb27bdab69b505d9bf71c99864051b37ac3de531d91fdad1598ad247138" "7e068da4ba88162324d9773ec066d93c447c76e9f4ae711ddd0c5d3863489c52" default))
+   '("ce4234c32262924c1d2f43e6b61312634938777071f1129c7cde3ebd4a3028da" "f64189544da6f16bab285747d04a92bd57c7e7813d8c24c30f382f087d460a33" "aec7b55f2a13307a55517fdf08438863d694550565dee23181d2ebd973ebd6b8" "e5a748cbefd483b74b183d7da4fca6228207a6bf9be9792dc85403a186724e1f" "9225db6c746ab38bdfe14af45aaf0549dc9d6bca73ffe81fd9e955f843d2e095" "68b35e92f9daa37685218bd11aa5307140a0ec4c8fd17142a83457619e7b1240" "bcfeecf5f2ee0bbc64450f7c5155145d8d2c590b1310a898c505f48b4b5f4c75" "02f57ef0a20b7f61adce51445b68b2a7e832648ce2e7efb19d217b6454c1b644" "467dc6fdebcf92f4d3e2a2016145ba15841987c71fbe675dcfe34ac47ffb9195" "51c71bb27bdab69b505d9bf71c99864051b37ac3de531d91fdad1598ad247138" "7e068da4ba88162324d9773ec066d93c447c76e9f4ae711ddd0c5d3863489c52" default))
+ '(org-agenda-files
+   '("/home/yaslam/Git/dotfiles-public/README.org" "/home/yaslam/org/todo.org"))
  '(org-safe-remote-resources
-   '("\\`https://fniessen\\.github\\.io/org-html-themes/org/readtheorg\\.setup\\'"))
+   '("\\`https://fniessen\\.github\\.io/org-html-themes/org/theme-ReadTheOrg\\.setup\\'" "\\`https://fniessen\\.github\\.io/org-html-themes/org/readtheorg\\.setup\\'"))
  '(smtpmail-smtp-server "smtp.gmail.com")
  '(smtpmail-smtp-service 25))
 (custom-set-faces
@@ -14,7 +16,15 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(font-lock-comment-face ((t (:slant italic))))
+ '(font-lock-keyword-face ((t (:slant italic)))))
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ 
+'(font-lock-comment-face ((t (:slant italic))))
+ '(font-lock-keyword-face ((t (:slant italic))))
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
