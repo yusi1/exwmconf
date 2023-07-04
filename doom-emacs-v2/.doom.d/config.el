b9985f31a2ad9c1a1cc3014bd1184311636c7250
Yusef Aslam <YUZi54780@outlook.com>
2023-07-04 22:01:07 +0100

    config.el: add 'emacs' use-package! section

--- doom-emacs-v2/.doom.d/config.el
+++ doom-emacs-v2/.doom.d/config.el
@@ -177,6 +177,22 @@
   ;; See also `corfu-exclude-modes'.
   :init
   (global-corfu-mode))
+
+;; A few more useful configurations...
+(use-package! emacs
+  :init
+  ;; TAB cycle if there are only few candidates
+  (setq completion-cycle-threshold 3)
+
+  ;; Emacs 28: Hide commands in M-x which do not apply to the current mode.
+  ;; Corfu commands are hidden, since they are not supposed to be used via M-x.
+  ;; (setq read-extended-command-predicate
+  ;;       #'command-completion-default-include-p)
+
+  ;; Enable indentation+completion using the TAB key.
+  ;; `completion-at-point' is often bound to M-TAB.
+  (setq tab-always-indent 'complete))
+
 (after! erc
   ;; Use authinfo instead of prompting for passwords.
   (setq erc-prompt-for-password nil)
