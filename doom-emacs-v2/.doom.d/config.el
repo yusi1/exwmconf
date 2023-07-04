4522c9d622e43f8acd7e59d032c17396fe50c1bb
Yusef Aslam <YUZi54780@outlook.com>
2023-07-04 22:00:43 +0100

    config.el: add 'corfu' use-package! section

--- doom-emacs-v2/.doom.d/config.el
+++ doom-emacs-v2/.doom.d/config.el
@@ -154,6 +154,29 @@
 
   (vertico-multiform-mode t))
 
+(use-package! corfu
+  ;; Optional customizations
+  ;; :custom
+  ;; (corfu-cycle t)                ;; Enable cycling for `corfu-next/previous'
+  ;; (corfu-auto t)                 ;; Enable auto completion
+  ;; (corfu-separator ?\s)          ;; Orderless field separator
+  ;; (corfu-quit-at-boundary nil)   ;; Never quit at completion boundary
+  ;; (corfu-quit-no-match nil)      ;; Never quit, even if there is no match
+  ;; (corfu-preview-current nil)    ;; Disable current candidate preview
+  ;; (corfu-preselect 'prompt)      ;; Preselect the prompt
+  ;; (corfu-on-exact-match nil)     ;; Configure handling of exact matches
+  ;; (corfu-scroll-margin 5)        ;; Use scroll margin
+
+  ;; Enable Corfu only for certain modes.
+  ;; :hook ((prog-mode . corfu-mode)
+  ;;        (shell-mode . corfu-mode)
+  ;;        (eshell-mode . corfu-mode))
+
+  ;; Recommended: Enable Corfu globally.
+  ;; This is recommended since Dabbrev can be used globally (M-/).
+  ;; See also `corfu-exclude-modes'.
+  :init
+  (global-corfu-mode))
 (after! erc
   ;; Use authinfo instead of prompting for passwords.
   (setq erc-prompt-for-password nil)
