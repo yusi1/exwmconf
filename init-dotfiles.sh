af89041bbe73002040e786f31642adb0963ad051
Yusef Aslam <YUZi54780@outlook.com>
2023-09-07 12:56:27 +0100

    Squashed 'dotfile-init-scripts/' changes from 48a3d89..d4f0492
    d4f0492 unlink-dotfiles.sh: remove uneeded checks, move exit to start
    d0566b3 init-dotfiles.sh: remove uneeded checks, move exit to start
    0225826 unlink-dotfiles.sh: add checking for 'stow'
    bd250c6 unlink-dotfiles.sh: add checking for dotfiles repo and git
    22f191d init-dotfiles.sh: remove check for 'dialog'
    
    git-subtree-dir: dotfile-init-scripts
    git-subtree-split: d4f0492985a9bffcabe23710e7adff012d509974

--- init-dotfiles.sh
+++ init-dotfiles.sh
@@ -1,37 +1,22 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 DOTFILES_REPO="git@git.sr.ht:~yusz/dotfiles-v2"
 DOTFILES="$HOME/.dotfiles"
 
-[[ $(which apt 2>/dev/null) ]] && PKGMGR="apt" DISTRO="ubuntu/debian"
-[[ $(which xbps-install 2>/dev/null) ]] && PKGMGR="xbps" DISTRO="void"
-[[ $(which pacman 2>/dev/null) ]] && PKGMGR="pacman" DISTRO="arch"
-
-case $DISTRO in
-	"ubuntu/debian")
-		INSTALLCMD="sudo apt install ";;
-	"void")
-		INSTALLCMD="sudo xbps-install -S ";;
-	"arch")
-		INSTALLCMD="sudo pacman -S ";;
-esac
-
 if [[ $(which git 2>/dev/null) ]]; then
 	[ ! -d $DOTFILES ] && git clone $DOTFILES_REPO $DOTFILES
 else
-	echo "git not found, installing using $PKGMGR"
-	echo $(${INSTALLCMD} "git")
+	echo "git not found, exiting."
 fi
 
-[[ $(which stow 2>/dev/null) ]] || echo $(${INSTALLCMD} "stow")
-[[ $(which dialog 2>/dev/null) ]] || echo $(${INSTALLCMD} "dialog")
+[[ $(which stow 2>/dev/null) ]] || echo "stow not found, exiting."
 
 cd $DOTFILES
 echo -e "\n[Select packages to install]"
 
 PACKAGES_LIST=()
 
-select SELECTION in $(find . -maxdepth 1 | sed 's/\.\///1' | sort | awk 'NR>1' && echo -e "\n[EXIT]")
+select SELECTION in $(echo "[EXIT]" && find . -maxdepth 1 | sed 's/\.\///1' | sort | awk 'NR>1')
 do
 	[[ $SELECTION == "[EXIT]" ]] && break
 	PACKAGES_LIST+=($SELECTION)
